efb20bd967c540cfc1360746de7773a874ac2ef5
smeea-2018 <smeeaarshad@gmail.com>
2022-05-08 09:03:15 +0100

    added working hours, get hour from moment , compare current hour with current block value function defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
+// Define object
+workingHoursArray = [
+  { timeLabel: "9am", key: 9 },
+  { timeLabel: "10am", key: 10 },
+  { timeLabel: "11am", key: 11 },
+  { timeLabel: "12pm", key: 12 },
+  { timeLabel: "1pm", key: 13 },
+  { timeLabel: "2pm", key: 14 },
+  { timeLabel: "3pm", key: 15 },
+  { timeLabel: "4pm", key: 16 },
+  { timeLabel: "5pm", key: 17 },
+];
 
-// Define array
-workingHoursArray =[
-    {label: “9am”, key: 9},
-    {label: “10am”, key: 10},
-    {label: “11am”, key: 11},
-    {label: “12pm”, key: 12},
-    {label: “1pm”, key: 13},
-    {label: “2pm”, key: 14},
-    {label: “3pm”, key: 15},
-    {label: “4pm”, key: 16},
-    {label: “5pm”, key: 17}
-]
 const renderTimeblock = () => {
+  var currentTime = moment().hour();
+  console.log(currentTime);
+
   var containerDiv = $("#time - block - container");
   var timerblockLabelDiv = $("<div>");
 
@@ -20,15 +23,35 @@ const renderTimeblock = () => {
   var timerblockLabel = $("<label>");
 
   timerblockLabel.attr("class", "timerblockLabel");
+  timerblockLabel.attr("data-value", 9);
 
   timerblockLabelDiv.append(timerblockLabel);
   containerDiv.append(timerblockLabelDiv);
   console.log("hi");
+
+  //compare data-value with current hour
+  if (timerblockLabel.data - value < currentTime) {
+    console.log("past");
+  }
+
+  $function(){
+    $("#time-block-button-9")
+  };
 };
 
-const renderDayPlanner = () =>{
-    GetDate();
-    renderTimeblock();
-}
+const displayDate = (dateToday) => {
+  $("#dateDiv").append($("<h2/>").text("h"));
+};
+
+const getDate = () => {
+  const dateToday = moment().format("MMM Do YY");
+  displayDate(dateToday);
+  console.log(dateToday);
+};
+
+const renderDayPlanner = () => {
+  getDate();
+  renderTimeblock();
+};
 
 $(window).on("load", renderDayPlanner);
