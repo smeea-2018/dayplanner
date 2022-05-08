041d0b69363e0b5cb2491a6a19e4a9dc3b910fc2
smeea-2018 <smeeaarshad@gmail.com>
2022-05-08 21:16:14 +0100

    dynamically rendered day planner (html label, textbox and buttons)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-// Define object
-workingHoursArray = [
+// Define global object
+const workingHoursArray = [
   { timeLabel: "9am", key: 9 },
   { timeLabel: "10am", key: 10 },
   { timeLabel: "11am", key: 11 },
@@ -11,63 +11,100 @@ workingHoursArray = [
   { timeLabel: "5pm", key: 17 },
 ];
 
-const storeInLS = () => {};
+const getFromLocalStorage = () => {
+  //get value from local storage
+  JSON.parse(localStorage.getItem("schedule"));
+};
 
-const renderTimeblock = () => {
-  var currentTime = moment().hour();
+const storeInLS = (schedule) => {
+  //if no value exists in local storage
+  if (!toDoList) {
+    //set to do list as null
+    localStorage.setItem("toDoList", JSON.stringify([]));
+  } else {
+    //get value from ls
+    getFromLocalStorage();
+
+    //append schedule value to array
+    //
+    //write in ls
+    localStorage.setItem("toDoList", JSON.stringify(schedule));
+  }
+};
+
+const renderTimeblock = (workingHoursArray) => {
+  //get current hour
+  const currentTime = moment().hour();
   console.log(currentTime);
 
-  var containerDiv = $("#time - block - container");
-  console.log(containerDiv);
+  //target container div
+  const containerDiv = $("row-p2");
 
-  var texArea = $("textarea-timeblock");
+  const timeBlock = `<div class="row p-2 my-2 ${getClassName(
+    workingHoursArray.key
+  )}">
+  <div class ="col-md-1 col-sm-12 text-center my-1 d-flex flex-column justify-content-center>${
+    workingHoursArray.timeLabel
+  }"></div>
+      <input type="text"
+          class="col-md-9 col-sm-12 border border-dark" data-value=${
+            workingHoursArray.key
+          } 
+         
+        > ${getEventForTimeBlock(workingHoursArray.key)}</input>
+        <div
+          class="col-md-2 col-sm-12 text-center my-1 d-flex flex-column justify-content-center"
+        >
+          <button type="button" data-hour=${
+            workingDay.key
+          }class="btn btn-success">Save</button>
+        </div>`;
 
-  console.log(texArea.value);
+  //append to parentdiv
+  containerDiv.append(timeBlock);
+};
+workingHoursArray.forEach(renderTimeblock);
 
-  /*var timerblockLabelDiv = $("<div>");
+const schedule = {
+  timeLabel,
+  textValue,
+};
 
-  timerblockLabelDiv.attr("class", "timeblock");
-
-  var timerblockLabel = $("<label>");
-
-  timerblockLabel.attr("class", "timerblockLabel");
-  timerblockLabel.attr("data-value", 9);
-
-  timerblockLabelDiv.append(timerblockLabel);
-  containerDiv.append(timerblockLabelDiv);*/
-
-  //compare data-value with current hour
-  /*if (timerblockLabel.data - value < currentTime) {
+//compare data-value with current hour
+/*if (timerblockLabel.data - value < currentTime) {
     console.log("past");
   }*/
 
-  // if(){}
+// if(){}
 
-  // $function(){
-  //   $("#time-block-button-9"). click(function()
-  //   //get value from text area
-  //   var textareaValue = $("time-block-text-9").value();
-  //   localStorage.setItem("toDoList",textareaValue);
-  //   )
-  // };
+// $function(){
+//   $("#time-block-button-9"). click(function()
+//   //get value from text area
+//   var textareaValue = $("time-block-text-9").value();
+//   localStorage.setItem("toDoList",textareaValue);
+//   )
+// };
 
-  containerDiv.click("storeInLS");
-};
-
-getFromLocalStorage = () => {};
+containerDiv.click("storeInLS");
+// };
 
 const displayDate = (dateToday) => {
-  $("#dateDiv").append($("<h2/>").text("h"));
+  $("#currentDay").empty().append(dateToday);
 };
 
 const getDate = () => {
+  //get date from moment
   const dateToday = moment().format("MMM Do YY");
+  //function called to display date
   displayDate(dateToday);
   console.log(dateToday);
 };
 
 const renderDayPlanner = () => {
+  // function to get date is called
   getDate();
+
+  //function to render timeblocks is called
   renderTimeblock();
 };
 
